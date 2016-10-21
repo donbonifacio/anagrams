581c904bf36793fc38e1819d34ab34474fdd79a6
Pedro Santos <donbonifacio@gmail.com>
2016-10-21 17:43:20 +0100

    Add stress tests

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aliases {"stress"  ["run" "-m" "anagrams.stress/biggg-test"]
+            "criterium" ["run" "-m" "anagrams.stress/criterium"]}
   :plugins [[com.jakemccrary/lein-test-refresh "0.17.0"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [criterium "0.4.4"]])
