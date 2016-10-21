b13034a49b3c67454f666adf0d7a1e95c9f1735f
Pedro Santos <donbonifacio@gmail.com>
2016-10-21 17:00:32 +0100

    First solution

--- src/anagrams/core.clj
+++ src/anagrams/core.clj
@@ -1,6 +1,17 @@
 (ns anagrams.core)
 
-(defn anagrams
-  "Returns a list of anagrams."
-  [context word]
-  {:matches []})
+(defn anagrams [context word]
+  (if (empty? word)
+    {:store (:store context) :matches []}
+    (let [word-index (clojure.string/join "" (sort (clojure.string/lower-case word)))
+          current-values (get-in context [:store word-index])
+          new-values (distinct (conj current-values word))]
+      (-> context
+          (assoc-in [:store word-index] new-values)
+          (assoc :matches (cond
+                            (zero? (count current-values))
+                              []
+                            (= 1 (count new-values))
+                              new-values
+                            :else
+                            (remove #(= % word) new-values)))))))
