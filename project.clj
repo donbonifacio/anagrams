5a51836c503ae8853efa4098f6d971c8ebf0bc12
Pedro Santos <donbonifacio@gmail.com>
2016-10-22 09:35:53 +0100

    Add find-anagrams

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aliases {"stress"  ["run" "-m" "anagrams.stress/biggg-test"]
+            "find-anagrams" ["run" "-m" "anagrams.stress/find-anagrams"]
             "criterium" ["run" "-m" "anagrams.stress/criterium"]}
   :plugins [[com.jakemccrary/lein-test-refresh "0.17.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
--- src/anagrams/stress.clj
+++ src/anagrams/stress.clj
@@ -11,6 +11,20 @@
     (println "Loading" (count words) "words...")
     (time (reduce anagrams {} words))))
 
+(defn find-anagrams [& args]
+  (let [words (get-words args)]
+    (println "Loading" (count words) "words...")
+    (time (reduce (fn [context word]
+                    (let [context (anagrams context word)
+                          matches (:matches context)]
+
+                      (when (> (count matches) 1)
+                        (println word " -> " matches))
+
+                      context))
+                  {}
+                  words))))
+
 (defn criterium [& args]
   (let [words (get-words args)]
     (println "Testing" (count words) "words...")
